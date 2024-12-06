d44f561612f256b42c8fd359302587cb6c192688
Damares Gaia <damaresgaia@gmail.com>
2024-12-05 21:46:46 -0300

    feat: Implementa formulário básico para cadastro de usuários no componente Home

--- pp-cadastro-usuarios/src/pages/Home/index.jsx
+++ pp-cadastro-usuarios/src/pages/Home/index.jsx
@@ -4,8 +4,25 @@ function Home() {
   
   return (
 
-      <div>
-        <h1>Olá react</h1>
+      <div className='container'>
+        <form>
+          <h1>Cadastro de Usuários</h1>
+          <input name='nome' type='text'/>
+          <input name='idade' type='number'/>
+          <input name='email' type="email"/>
+          <button type='button'>Cadastrar</button>
+        </form>
+
+        <div>
+          <div>
+            <p>Nome: </p>
+            <p>Idade: </p>
+            <p>Email: </p>
+          </div>
+          <button>
+            <img />
+          </button>
+        </div>
       </div>
   )
 }
