93c786fd7b28dde369f12f9ddcd82e98e6bd6fae
Damares Gaia <damaresgaia@gmail.com>
2024-12-09 21:27:06 -0300

    feat: Implementa endpoints para criar e listar usuários com armazenamento em memória

--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 const users = []
 
 app.post('/usuarios', (req, res) => {
 
-    console.log(req)
+    users.push(req.body)
 
-    res.send('Ok post')
+    res.status(201).json(req.body)
 })
 
 app.get('/usuarios', (req, res) => {
-    res.send('Ok, deu bom')
+    res.status(200).json(users)
 })
 
 app.listen(3000)
